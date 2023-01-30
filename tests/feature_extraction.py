a80f4c2cbe73ea37813b690e9806c665ed693c85
imilyutin <idmilyutin@gmail.com>
2023-01-30 12:10:40 +0400

    added test to verify implementation of WeightedAverageWordVectors

--- tests/feature_extraction.py
+++ tests/feature_extraction.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
 from gensim import downloader
-from news_tweets_analysis.feature_extraction import AverageWordVectors
+from news_tweets_analysis.feature_extraction import (
+    AverageWordVectors,
+    WeightedAverageWordVectors
+)
 
 WORD_VECTORS_NAME = 'glove-wiki-gigaword-50'
 
@@ -19,3 +22,23 @@ def test_average_word_vectors_single_doc(word_vectors):
         average_vector,
         (word_vectors['i'] + word_vectors['am'] + word_vectors['working'])/3
     ).all()
+
+
+def test_average_word_vectors_multiple_docs(word_vectors):
+    averager = AverageWordVectors(model=word_vectors)
+    docs = ["I'm working", "You're working"]
+    average_vectors = averager.fit_transform(docs)
+    assert average_vectors.shape == (2, 50)
+
+
+def test_weighted_average_word_vectors_multiple_docs(word_vectors):
+    averager = WeightedAverageWordVectors(model=word_vectors)
+    docs = ["I am working", "You are working here"]
+    average_vectors = averager.fit_transform(docs)
+    true_vector = word_vectors.get_mean_vector(
+        keys=['am', 'i', 'working'],
+        weights = [0.6316672, 0.6316672, 0.44943642],
+        pre_normalize=False
+    )
+    assert average_vectors.shape == (2, 50)
+    assert np.isclose(average_vectors[0,:], true_vector).all()
